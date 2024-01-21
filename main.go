13e0316e30df0cdfd480f0d026d050508857239a
Elias Gill <eliasgill42@gmail.com>
2024-01-21 14:44:43 -0300

    feat(repl): extending repl for parse mode and lexer mode

--- main.go
+++ main.go
@@ -3,11 +3,34 @@ package main
 import (
 	"fmt"
 	"os"
+    "flag"
 
 	"github.com/sl2.0/repl"
 )
 
 func main() {
-	fmt.Println("Starting REPL...")
-	repl.Start(os.Stdin, os.Stdout)
+    const colorMagenta = "\033[35m"
+    const colorNone = "\033[0m"
+
+    // Define flags
+    parserFlag := flag.Bool("parser", false, "Enable parser flag")
+    lexerFlag := flag.Bool("lexer", false, "Enable lexer flag")
+
+    // Parse command-line flags
+    flag.Parse()
+
+    fmt.Print("\nStarting REPL ")
+
+    // Check if either --parser or --lexer flag is provided
+    if *parserFlag {
+        fmt.Printf("in%s parser%s mode:\n", colorMagenta, colorNone)
+        repl.StartParserREPL(os.Stdin, os.Stdout)
+        return
+    }
+
+    if *lexerFlag {
+        fmt.Printf("in%s lexer%s mode:\n", colorMagenta, colorNone)
+        repl.StartLexerREPL(os.Stdin, os.Stdout)
+        return
+    }
 }
--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,11 @@ import (
 	"io"
 
 	"github.com/sl2.0/lexer"
+	"github.com/sl2.0/parser"
 	"github.com/sl2.0/tokens"
 )
 
-func Start(in io.Reader, out io.Writer) {
+func StartLexerREPL(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	// read with an infinit loop
@@ -33,5 +34,40 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		fmt.Print("\n\n")
+	}
+}
+
+func StartParserREPL(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+        fmt.Print(">>> ")
+
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+        line := scanner.Text()
+        if line == "exit" {
+            return
+        }
+
+		p := parser.NewParser(line)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.ToString())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
